cee4e4761cf9775347858910202b13a7844542b5
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-15 20:51:41 +0530

    Add taskList-2

---
+++ Employee-Management-System/src/Component/TaskList/Tasklist2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tasklist2 = () => {
+  return (
+    <div className='h-[40%]'>
+
+    </div>
+  )
+}
+
+export default Tasklist2
--- Employee-Management-System/src/Component/other/TaskList.jsx
+++ Employee-Management-System/src/Component/other/TaskList.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 const TaskList = () => {
   return (
     <div className='flex flex-wrap lg:flex-nowrap mt-10 justify-between gap-5 screen '>
-       <div className='rounded-xl w-[35%] py-6 px-9  bg-red-400'>
+       <div className='rounded-xl w-[35%] py-6 px-9 active:scale-90 bg-red-400'>
            <h2 className='text-3xl font-semibold'>0</h2>
              <h3 className='text-xl font-medium'>new task</h3>
        </div>
-       <div className='rounded-xl w-[35%] py-6 px-9  bg-blue-400'>
+       <div className='rounded-xl w-[35%] py-6 px-9 active:scale-90  bg-blue-400'>
            <h2 className='text-3xl font-semibold'>0</h2>
              <h3 className='text-xl font-medium'>new task</h3>
        </div>
-       <div className='rounded-xl w-[35%] py-6 px-9  bg-green-300'>
+       <div className='rounded-xl w-[35%] py-6 px-9 active:scale-90 bg-green-300'>
            <h2 className='text-3xl font-semibold'>0</h2>
              <h3 className='text-xl font-medium'>new task</h3>
        </div>
-       <div className='rounded-xl w-[35%] py-6 px-9  bg-yellow-400'>
+       <div className='rounded-xl w-[35%] py-6 px-9 active:scale-90  bg-yellow-400'>
            <h2 className='text-3xl font-semibold'>0</h2>
              <h3 className='text-xl font-medium'>new task</h3>
        </div>
