b62f35bdc1e29ebbf7a7da915f7314d5a47e0c23
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-15 16:00:14 +0530

    Git add LOgpage structure

--- Employee-Management-System/src/Component/Auth/Login.jsx
+++ Employee-Management-System/src/Component/Auth/Login.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='flex h-screen w-screen items-center justify-center'>
+      <div>
+        <form >
+            <input type="text" placeholder='Enter your email' />
+            <input type="text" placeholder='Enter password' />
+            <button>Log in</button>
+        </form>
+      </div>
+
+
+    </div>
   )
 }
 
