708c68d7b670642e94396a157ba7149cb4cb789d
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-15 20:44:44 +0530

    Tasklist

--- Employee-Management-System/src/Component/Dashboad/EmployeeDash.jsx
+++ Employee-Management-System/src/Component/Dashboad/EmployeeDash.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../other/Header'
+import TaskList from '../other/TaskList'
 
 const EmployeeDash = () => {
   return (
     <div className='p-10 bg-[#1C1C1C] h-screen'>
         <Header />
+        <TaskList />
     </div>
   )
 }
