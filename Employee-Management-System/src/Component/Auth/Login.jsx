1745160d258c8c851a0b21f8bc820756ad3cbdcb
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-15 16:42:15 +0530

    Add e.preventDefault

--- Employee-Management-System/src/Component/Auth/Login.jsx
+++ Employee-Management-System/src/Component/Auth/Login.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 
 const Login = () => {
+
+  const submitHandler = (e) =>{
+      e.preventDefault()
+      console.log('submit dale');
+      
+  }
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
       <div className='border-2 rounded-2xl p-20  border-emerald-600'>
-        <form className='flex flex-col items-center justify-center'>
+        <form onSubmit={(e)=>{
+            submitHandler(e)
+        }}
+        className='flex flex-col items-center justify-center'>
             <input required className='border-2 border-emerald-500 rounded-full bg-transparent outline-none text-xl py-3 px-5 placeholder:text-gray-400 ' 
             type="email" placeholder='Enter your email' />
             <input required className='border-2 border-emerald-500 rounded-full bg-transparent mt-4 outline-none text-xl py-3 px-5 placeholder:text-gray-500 '  type="password" placeholder='Enter password' />
-            <button className='border-none mt-5 bg-emerald-500 rounded-full  text-white outline-none text-xl py-2 px-4 placeholder:text-gray-400 ' >Log in</button>
+            <button className='border-none mt-5 bg-emerald-500 rounded-full  text-white outline-none font-bold text-xl py-2 px-5 cursor-pointer active:scale-110 hover:bg-pink-100 hover:text-pink-300 ' >Log in</button>
         </form>
       </div>
 
