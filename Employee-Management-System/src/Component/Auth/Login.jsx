7b35538cbab1130c9ed37250f984bb694c0e5622
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-16 22:45:59 +0530

    add responsible for all device css

--- Employee-Management-System/src/Component/Auth/Login.jsx
+++ Employee-Management-System/src/Component/Auth/Login.jsx
@@ -13,8 +13,8 @@ const Login = () => {
 
   }
   return (
-    <div className='flex h-screen w-screen items-center justify-center'>
-      <div className='border-2 rounded-4xl p-20  border-emerald-600'>
+    <div className='flex h-screen w-full lg:w-screen items-center justify-center '>
+      <div className='border-2 rounded-4xl p-10 lg:p-20 h-1/2  border-emerald-600  '>
         <form onSubmit={(e) => {
           submitHandler(e)
         }}
@@ -24,15 +24,15 @@ const Login = () => {
             onChange={(e) => {
               setgmail(e.target.value)
             }}
-            required className='border-2 border-emerald-500 rounded-full bg-transparent outline-none text-xl py-3 px-5 placeholder:text-gray-400 '
+            required className='border-2 border-emerald-500 rounded-full bg-transparent outline-none text-xl py-3 px-5 placeholder:text-gray-400 active:scale-95  hover:bg-pink-100 '
             type="email" placeholder='Enter your email' />
           <input
             value={password}
             onChange={(e) => {
               setpassword(e.target.value)
             }}
-            required className='border-2 border-emerald-500 rounded-full bg-transparent mt-4 outline-none text-xl py-3 px-5 placeholder:text-gray-500 ' type="password" placeholder='Enter password' />
-          <button className='border-none mt-5 bg-emerald-500 rounded-full  text-white outline-none font-bold text-xl py-2 px-8 cursor-pointer active:scale-110 hover:bg-pink-100 hover:text-pink-300 ' >Log in</button>
+            required className='border-2 border-emerald-500 rounded-full bg-transparent lg:mt-5 mt-15 outline-none text-xl py-3 px-5 placeholder:text-gray-500 active:scale-95   hover:bg-pink-200' type="password" placeholder='Enter password' />
+          <button className='border-none lg:mt-5 mt-15 bg-emerald-500 rounded-full  text-white outline-none font-bold text-xl py-2 px-8 cursor-pointer active:scale-50 hover:bg-pink-300 hover:text-pink-100 ' >Log in</button>
         </form>
       </div>
 
