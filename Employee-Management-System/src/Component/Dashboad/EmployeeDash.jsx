86b3c20489835e923c9bd288120348ee87bacf78
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-16 22:02:30 +0530

    taklist info

--- Employee-Management-System/src/Component/Dashboad/EmployeeDash.jsx
+++ Employee-Management-System/src/Component/Dashboad/EmployeeDash.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../other/Header'
 import TaskList from '../other/TaskList'
+import Tasklist2 from '../TaskList/Tasklist2'
 
 const EmployeeDash = () => {
   return (
     <div className='p-10 bg-[#1C1C1C] h-screen'>
         <Header />
         <TaskList />
+        <Tasklist2 />
     </div>
   )
 }
--- Employee-Management-System/src/Component/TaskList/Tasklist2.jsx
+++ Employee-Management-System/src/Component/TaskList/Tasklist2.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Tasklist2 = () => {
   return (
-    <div className='h-[40%]'>
-
+    <div id='tasklist' className='flex h-[50%] overflow-auto lg:overflow-x-auto  items-center justify-start gap-5 flex-wrap lg:flex-nowrap py-5  w-full  mt-10 '>
+       <div className='shrink-0 lg:h-full h-40 w-80 bg-yellow-400 rounded-xl'>
+        
+        </div> 
+      
     </div>
   )
 }
