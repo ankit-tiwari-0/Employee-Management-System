9ce7a0c43d559ea7b5b479a94fd6ca63afc6de58
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-17 19:12:55 +0530

    Structure of ED

--- Employee-Management-System/src/Component/Dashboad/AdminDashboad.jsx
+++ Employee-Management-System/src/Component/Dashboad/AdminDashboad.jsx
@@ -3,8 +3,23 @@ import Header from '../other/Header'
 
 const AdminDashboad = () => {
   return (
-    <div>
+    <div className='h-screen w-full p-10'>
         <Header />
+        <div>
+            <form>
+                <h3> Task Title</h3>
+                <input type="text" placeholder='make ' />
+                <h3>Description</h3>
+                <textarea name="" id=" " cols="30" rows="10"></textarea>
+                <h3>Date</h3>
+                <input type="text" />
+                <h3>Asign to </h3>
+                <input type="text" placeholder='Employee name' />
+                <h3>Cateogory</h3>
+                <input type="text" placeholder='Design, dev etc' />
+                <button>Create Task</button>
+            </form>
+        </div>
     </div>
   )
 }
