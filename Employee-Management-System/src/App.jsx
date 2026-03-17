7d1f37f91e404d5de4ff2cc43735d99cb21577bd
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-17 19:03:44 +0530

    git add AdminDashboad

--- Employee-Management-System/src/App.jsx
+++ Employee-Management-System/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Login from './Component/Auth/Login'
 import EmployeeDash from './Component/Dashboad/EmployeeDash'
+import AdminDashboad from './Component/Dashboad/AdminDashboad'
 
 const App = () => {
   return (
    <>
    {/* <Login /> */}
-   <EmployeeDash />
+   {/* <EmployeeDash /> */}
+   <AdminDashboad />
    </>
   )
 }
