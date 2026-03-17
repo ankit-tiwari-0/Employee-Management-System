d11ce87c8d65a6821a51185ad558c8fd483f228e
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-17 22:11:00 +0530

    localstorage

--- Employee-Management-System/src/App.jsx
+++ Employee-Management-System/src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './Component/Auth/Login'
 import EmployeeDash from './Component/Dashboad/EmployeeDash'
 import AdminDashboad from './Component/Dashboad/AdminDashboad'
+import { setLocalStorage } from './utils/localstorage'
 
 const App = () => {
+
+  useEffect(() => {
+    setLocalStorage()
+  
+  },)
+  
   return (
    <>
    <Login />
