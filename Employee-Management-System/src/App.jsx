03c4b9cfeaf8282a5eb0144690b5531bacb4a621
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-17 22:19:34 +0530

    getlocalstoragge

--- Employee-Management-System/src/App.jsx
+++ Employee-Management-System/src/App.jsx
@@ -2,14 +2,9 @@ import React, { useEffect } from 'react'
 import Login from './Component/Auth/Login'
 import EmployeeDash from './Component/Dashboad/EmployeeDash'
 import AdminDashboad from './Component/Dashboad/AdminDashboad'
-import { setLocalStorage } from './utils/localstorage'
+import { getLocalStorage, setLocalStorage } from './utils/localstorage'
 
 const App = () => {
-
-  useEffect(() => {
-    setLocalStorage()
-  
-  },)
   
   return (
    <>
--- Employee-Management-System/src/utils/localstorage.jsx
+++ Employee-Management-System/src/utils/localstorage.jsx
@@ -201,3 +201,11 @@ export const setLocalStorage = ()=>{
     localStorage.setItem('employee', JSON.stringify(employees))
     localStorage.setItem('admin', JSON.stringify(admin))
 }
+
+export const getLocalStorage = ()=>{
+    const employees = JSON.parse(localStorage.getItem('employees')) 
+    const admin = JSON.parse(localStorage.getItem('admin')) 
+
+    console.log(employees,admin);
+    
+}
