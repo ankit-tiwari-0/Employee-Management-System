100cc5c6b0191500398d0ecf2d67086e32793e0c
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-26 20:13:01 +0530

    logic

--- Employee-Management-System/src/App.jsx
+++ Employee-Management-System/src/App.jsx
@@ -1,18 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './Component/Auth/Login'
 import EmployeeDash from './Component/Dashboad/EmployeeDash'
 import AdminDashboad from './Component/Dashboad/AdminDashboad'
 import { getLocalStorage, setLocalStorage } from './utils/localstorage'
 
 const App = () => {
+  const [user, setuser] = useState(null)
+
+  const handleLogin = (email, password)=>{
+    if(email == 'admin@me.com' && password == '123'){
+      console.log('This is admin');
+      
+    } else if (email == 'user@me.com' && password == '123'){
+      console.log('This is user');
+      
+    }else{
+      alert('Invalid Credential')
+    }  
+
+  }
+
+  handleLogin('admin@me.com',123)
   
   return (
    <>
-   <Login />
+  {!user ?  <Login />: ''}
    {/* <EmployeeDash /> */}
    {/* <AdminDashboad /> */}
    </>
   )
 }
 
-export default App
+export default App 
