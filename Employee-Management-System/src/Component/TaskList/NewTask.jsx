d2d72a403013ad3e6ffafd7ea5d9d7ffd43de5c3
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-30 00:05:03 +0530

    structure of complte task

--- Employee-Management-System/src/Component/TaskList/NewTask.jsx
+++ Employee-Management-System/src/Component/TaskList/NewTask.jsx
@@ -10,7 +10,7 @@ const NewTask = () => {
         <h2 className='lg:mt-5 mt-1 text-2xl font-semibold'>Make A Video</h2>
         <p className='text-sm mt-1 lg:mt-3 md:line-clamp-none wrap-break-word line-clamp-3'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates soluta obcaecati vero numquam autem sit.dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</p>
         <div className='lg:mt-15 m-4'>
-            <button className='bg-green-500 py-1 px-2 text-l rounded-xl active:scale-50'>Accept</button>
+            <button className='bg-green-500 py-1 px-2 text-l rounded-xl active:scale-50 cursor-pointer'>Accept</button>
         </div>
         </div> 
   )
--- Employee-Management-System/src/Component/TaskList/Tasklist2.jsx
+++ Employee-Management-System/src/Component/TaskList/Tasklist2.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
 import AcceptTask from './AcceptTask'
 import NewTask from './NewTask'
+import CompleteTask from './CompleteTask'
 
 const Tasklist2 = ({data}) => {
   return (
     <div id='tasklist' className='flex h-[50%] overflow-auto lg:overflow-x-auto  items-center lg:justify-start justify-center gap-5 flex-wrap lg:flex-nowrap py-5  w-full  mt-10 '>
        <AcceptTask />
       <NewTask/>
-       <div className='shrink-0 lg:h-full h-40 lg:w-80 w-full p-5 bg-pink-400 rounded-xl'>
-        <div className='flex justify-between items-center'>
-             <h3 className=' text-sm px-3 py-1 rounded bg-red-600 active:scale-90'>High</h3>
-             <h4 className='text-sm bg-transparent'>DD-MM-YY</h4>
-        </div>
-        <h2 className='lg:mt-5 mt-1 text-2xl font-semibold'>Make A Video</h2>
-        <p className='text-sm mt-1 lg:mt-3 md:line-clamp-none wrap-break-word line-clamp-3'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates soluta obcaecati vero numquam autem sit.dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</p>
-        </div> 
+      <CompleteTask />
        <div className='shrink-0 lg:h-full h-40 lg:w-80 w-full p-5 bg-purple-400 rounded-xl'>
         <div className='flex justify-between items-center'>
              <h3 className=' text-sm px-3 py-1 rounded bg-red-600 active:scale-90'>High</h3>
