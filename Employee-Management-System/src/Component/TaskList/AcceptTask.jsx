6d21f548e6dc1bfc46ad612f019149d998f02f69
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-30 00:00:13 +0530

    comleted

--- Employee-Management-System/src/Component/TaskList/AcceptTask.jsx
+++ Employee-Management-System/src/Component/TaskList/AcceptTask.jsx
@@ -10,8 +10,8 @@ const AcceptTask = () => {
         <h2 className='lg:mt-5 mt-1 text-2xl font-semibold'>Make A Video</h2>
         <p className='text-sm mt-1 lg:mt-3 md:line-clamp-none wrap-break-word line-clamp-3'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates soluta obcaecati vero numquam autem sit.dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</p>
         <div className='flex justify-between lg:mt-15 m-4'>
-           <button className='bg-green-500 py-1 px-2 text-sm'> Completed</button> 
-           <button className='bg-red-500 py-0.5 px-1 text-sm'> Failed</button> 
+           <button className='bg-green-500 py-1 px-2 text-l rounded-xl'> Completed</button> 
+           <button className='bg-red-500 py-0.5 px-2 text-l rounded-xl'> Failed</button> 
         </div>
         </div> 
   )
