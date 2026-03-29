a98d466b932944f9988282c8b6c7037bcd49833f
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-30 00:20:39 +0530

    add data in task of empl

--- Employee-Management-System/src/Component/TaskList/Tasklist2.jsx
+++ Employee-Management-System/src/Component/TaskList/Tasklist2.jsx
@@ -7,10 +7,20 @@ import FailedTasl from './FailedTasl'
 const Tasklist2 = ({data}) => {
   return (
     <div id='tasklist' className='flex h-[50%] overflow-auto lg:overflow-x-auto  items-center lg:justify-start justify-center gap-5 flex-wrap lg:flex-nowrap py-5  w-full  mt-10 '>
-       <AcceptTask />
-      <NewTask/>
-      <CompleteTask />
-      <FailedTasl />
+      {data.tasks.map((elem)=>{
+          if(elem.active){
+               return <AcceptTask />
+          }
+          if(elem.newTask){
+               return <NewTask />
+          }
+          if(elem.completed){
+               return <CompleteTask />
+          }
+          if(elem.failed){
+               return <FailedTasl />
+          }
+      })}
       
     </div>
   )
