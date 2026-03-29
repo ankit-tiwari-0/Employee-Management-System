62e67473dbb21f24f0b0e17f2d060b9bc6ad591e
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-30 00:11:35 +0530

    Failed task

--- Employee-Management-System/src/Component/TaskList/Tasklist2.jsx
+++ Employee-Management-System/src/Component/TaskList/Tasklist2.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AcceptTask from './AcceptTask'
 import NewTask from './NewTask'
 import CompleteTask from './CompleteTask'
+import FailedTasl from './FailedTasl'
 
 const Tasklist2 = ({data}) => {
   return (
@@ -9,22 +10,7 @@ const Tasklist2 = ({data}) => {
        <AcceptTask />
       <NewTask/>
       <CompleteTask />
-       <div className='shrink-0 lg:h-full h-40 lg:w-80 w-full p-5 bg-purple-400 rounded-xl'>
-        <div className='flex justify-between items-center'>
-             <h3 className=' text-sm px-3 py-1 rounded bg-red-600 active:scale-90'>High</h3>
-             <h4 className='text-sm bg-transparent'>DD-MM-YY</h4>
-        </div>
-        <h2 className='lg:mt-5 mt-1 text-2xl font-semibold'>Make A Video</h2>
-        <p className='text-sm mt-1 lg:mt-3 md:line-clamp-none wrap-break-word line-clamp-3'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates soluta obcaecati vero numquam autem sit.dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</p>
-        </div> 
-       <div className='shrink-0 lg:h-full h-40 lg:w-80 w-full p-5 bg-indigo-400 rounded-xl'>
-        <div className='flex justify-between items-center'>
-             <h3 className=' text-sm px-3 py-1 rounded bg-red-600 active:scale-90'>High</h3>
-             <h4 className='text-sm bg-transparent'>DD-MM-YY</h4>
-        </div>
-        <h2 className='lg:mt-5 mt-1 text-2xl font-semibold'>Make A Video</h2>
-        <p className='text-sm mt-1 lg:mt-3 md:line-clamp-none wrap-break-word line-clamp-3'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates soluta obcaecati vero numquam autem sit.dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</p>
-        </div> 
+      <FailedTasl />
       
     </div>
   )
