446edf03a4cef861fa1dd2f6075ee6ea1b3cf6ba
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-29 23:50:16 +0530

    newtask structure is ready

--- Employee-Management-System/src/Component/TaskList/NewTask.jsx
+++ Employee-Management-System/src/Component/TaskList/NewTask.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const NewTask = () => {
   return (
-    <div>NewTask</div>
+    <div className='shrink-0 lg:h-full h-40 lg:w-80 w-full p-5 bg-teal-400 rounded-xl'>
+        <div className='flex justify-between items-center'>
+             <h3 className=' text-sm px-3 py-1 rounded bg-red-600 active:scale-90'>High</h3>
+             <h4 className='text-sm bg-transparent'>DD-MM-YY</h4>
+        </div>
+        <h2 className='lg:mt-5 mt-1 text-2xl font-semibold'>Make A Video</h2>
+        <p className='text-sm mt-1 lg:mt-3 md:line-clamp-none wrap-break-word line-clamp-3'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates soluta obcaecati vero numquam autem sit.dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</p>
+        </div> 
   )
 }
 
