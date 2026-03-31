e36a6a86244671dd97932698585e46ac87200203
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-31 22:02:43 +0530

    logic section is completed

--- Employee-Management-System/src/Component/TaskList/CompleteTask.jsx
+++ Employee-Management-System/src/Component/TaskList/CompleteTask.jsx
@@ -9,8 +9,10 @@ const CompleteTask = ({data}) => {
         </div>
         <h2 className='lg:mt-5 mt-1 text-2xl font-semibold'>{data.title}</h2>
         <p className='text-sm mt-1 lg:mt-3 md:line-clamp-none wrap-break-word line-clamp-3'>{data.description}</p>
-        <div className=' lg:mt-15 m-4'>
-            <button className='w-full'>Complete</button>
+        <div className=' lg:mt-15 m-4 flex justify-center items-center mt-6'>
+          <button className="border-2 border-gray-300 px-5 py-1 rounded-xl text-white font-semibold text-lg hover:bg-white hover:text-pink-500 transition-all duration-200 active:scale-95">
+    Complete
+  </button>
         </div>
         </div> 
   )
